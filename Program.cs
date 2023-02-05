6c19ae5c92a2f8ae20d024f44b63af0a63e6714e
asphyxiaseizurebeyondgodlike <121315533+asphyxiaseizurebeyondgodlike@users.noreply.github.com>
2023-02-05 23:32:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿class Person
+{
+    string FullName;
+    int age;
+
+    public void move()
+    {
+        Console.WriteLine($"Cейчас говорит {FullName}") ;
+    }
+
+    public void talk()
+    {
+        Console.WriteLine($"Cейчас говорит {FullName}");
+    }
+    public Person() 
+    { 
+
+    }
+     
+    public Person(string FullName, int age)
+    {
+        this.FullName = FullName ;
+        this.age = age;
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        Person person = new Person("Andew",12);
+        Person person1= new Person();
+
+        person.move();
+        person1.talk();
+    }
+
+}
